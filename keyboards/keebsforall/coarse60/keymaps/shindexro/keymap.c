58738c3494ce2f1c9562ea6b1ab05f2d1ac0508a
shindexro <shindexro@gmail.com>
2022-09-26 22:20:53 +0100

    coarse60: align keymap

--- keyboards/keebsforall/coarse60/keymaps/shindexro/keymap.c
+++ keyboards/keebsforall/coarse60/keymaps/shindexro/keymap.c
@@ -35,11 +35,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [LY_NAV] = LAYOUT_alice(
-    _______,  _______, KC_F1,     KC_F2,        KC_F3,       KC_F4,     KC_F5,     KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,     KC_F11,   KC_F12,  KC_DEL,
+    _______,  _______, KC_F1,     KC_F2,        KC_F3,       KC_F4,     KC_F5,     KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,   KC_F12,  KC_DEL,
     _______,  _______, A(KC_F4),  KC_MPRV,     KC_MPLY,      KC_MNXT,   C(S(KC_T)),         KC_ESC , _______, _______,  _______, _______,  _______, _______, _______,
-    _______,  _______, G(S(KC_S)), G(S(KC_S)),    ALT_TAB,     GUI_SPACE, SPAM_RIGHT,       KC_LEFT, KC_DOWN, KC_UP   , KC_RGHT,    _______,  _______,          _______,
-              _______, C(KC_Z),   C(KC_X),      C(KC_C),     C(KC_V),   SPAM_LEFT,          _______, _______, _______,  _______,    _______,  _______, _______, _______,
-              _______,            _______,      _______,     _______,                                   _______,                  _______,           QK_BOOT
+    _______,  _______, G(S(KC_S)), G(S(KC_S)),    ALT_TAB,     GUI_SPACE, SPAM_RIGHT,       KC_LEFT, KC_DOWN, KC_UP   , KC_RGHT, _______,  _______,          _______,
+              _______, C(KC_Z),   C(KC_X),      C(KC_C),     C(KC_V),   SPAM_LEFT,          _______, _______, _______,  _______, _______,  _______, _______, _______,
+              _______,            _______,      _______,     _______,                                _______,                    _______,           QK_BOOT
   ),
 
   [LY_NUM] = LAYOUT_alice(
